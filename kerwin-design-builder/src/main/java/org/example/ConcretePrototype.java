5938f76ddc3962d9690444fde549b5f276f3fcd8
zyc880321 <yuchen.zhao@gaeamobile.com>
2020-03-05 10:51:41 +0800

    提交代码

---
+++ kerwin-design-builder/src/main/java/org/example/ConcretePrototype.java
@@ -0,0 +1,84 @@
+package org.example;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tom.
+ */
+@Data
+public class ConcretePrototype implements Cloneable,Serializable {
+
+    private int age;
+    private String name;
+    private List<String> hobbies;
+
+    @Override
+    public ConcretePrototype clone() {
+        try {
+            return (ConcretePrototype)super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ConcretePrototype deepCloneHobbies(){
+        try {
+            ConcretePrototype result = (ConcretePrototype)super.clone();
+            result.hobbies = (List)((ArrayList)result.hobbies).clone();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 二进制
+     * @return
+     */
+    public ConcretePrototype byteDeepClone(){
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+
+            return (ConcretePrototype)ois.readObject();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * json
+     * @return
+     */
+    public ConcretePrototype jsonDeepClone(){
+        try {
+            return JSONObject.toJavaObject((JSON)JSONObject.toJSON(this), ConcretePrototype.class);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ConcretePrototype{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", hobbies=" + hobbies +
+                '}';
+    }
+}
